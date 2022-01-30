763a5a8a5ba8826e4cecc9022323cce8d8894b9f
Rajnish Kumar Jha <96009467+undefined@users.noreply.github.com>
2022-01-30 06:27:26 +0000

    First commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# lwc-helloworld-app
-This is my first hello world app by using LWC framework.
---
+++ src/app.js
@@ -0,0 +1,6 @@
+import {LightningElement} from 'lwc';
+
+export default class App extends LightningElement{
+  message="Hello World!";
+
+}
